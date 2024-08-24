5a32ae6370c2f96150b0c1b3785c0e3f838c7d07
mahesh <maheshtanneru2@gmail.com>
2024-08-24 15:33:53 -0400

    changed

--- source_code/server/index.js
+++ source_code/server/index.js
@@ -24,7 +24,7 @@ mongoose.connect(MONGODB_URI, {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB Atlas !!');
+  console.log('Connected to MongoDB Atlas');
 });
 
 app.use(express.json());
